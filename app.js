db92b70e6b934de1a1639a5132860a61ca5b31b1
ajcudd <ajcuddeback@gmail.com>
2020-12-11 15:42:50 -0500

    added validation for names

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const inquirer = require('inquirer');
 
+// declaring the prompt user method
 const promptUser = () => {
+    //returns the inquirer prompt
     return inquirer.prompt([
+        // Array of objects for each question
         {
+            // Objects that are required for inquirer
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            // validation function
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub username?'
+            message: 'Enter your GitHub username? (Required)',
+            // validation function
+            validate: gitNameInput => {
+                if (gitNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -20,16 +42,19 @@ const promptUser = () => {
     ]);
 };
 
-
+// Prompt method with argument of portfolioData to store the data of the entire portfolio
 const promptProject = portfolioData => {
+    // Will show the user this is a new project they are adding
     console.log(`
     ==================
     Add a New Project
     ==================
     `);
+    // If nothing is currently stored in the portfolioData.projects array - then create and empty array
     if (!portfolioData.projects) {
         portfolioData.projects = [];
     }
+    // return the project prompts
     return inquirer.prompt([
         {
             type: 'input',
@@ -39,7 +64,16 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            // validation function
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -50,7 +84,16 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your project. (Required)'
+            message: 'Enter the Github link to your project. (Required)',
+            // validation function
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project link!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
@@ -65,17 +108,24 @@ const promptProject = portfolioData => {
             default: false
         }
     ])
+        // then take the projects data (^^^ everything above)...
         .then(projectData => {
+            // and push the projectData into the portfolioData.projects array
             portfolioData.projects.push(projectData);
+            // if the user confirmed they want to add another project...
             if (projectData.confirmAddProject) {
+                // run the prompt project function again with the portfolio data
                 return promptProject(portfolioData);
+                // else...
             } else {
+                // return the portfolioData (The array)
                 return portfolioData;
             }
         })
 }
-
+//Begin with prompting the user for their name, github, and about them
 promptUser()
+    // once finished - call the promptProject function
     .then(promptProject)
     .then(portfolioData => {
         console.log(portfolioData)
